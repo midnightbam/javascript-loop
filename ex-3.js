f32a9202831e3e5f2014425430e0e3bc17032483
Your NameKritsiri <your.email@example.comkritsiri.nd@gmail.com>
2025-07-21 14:29:48 +0700

    update-ex

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,10 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
-
+for (let i =0;i<scores.length;i++){
+  if (minScore===undefined||minScore>scores[i]){
+    minScore =scores[i];
+  }
+  
+}
 console.log(minScore);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,7 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+for (let i = companyName.length - 1; i >= 0; i=i-1) {
+  reversedCompanyName = reversedCompanyName + companyName[i]; // เอาตัวอักษรทีละตัวมาใส่ต่อท้าย
+}
 console.log(reversedCompanyName);
